7ef58f07342d6b85222d7eb92bd215feae9ee20e
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-02 20:43:46 +0100

    update

--- Program.cs
+++ Program.cs
@@ -5,9 +5,26 @@ namespace _8_aspnetcore_configuration
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.Map("/", async context => {
+                    await context.Response.WriteAsync(app.Configuration["mYkEY"] + "\n");
+
+                    await context.Response.WriteAsync(app.Configuration.GetValue<string>("MyKey"));
+
+                    await context.Response.WriteAsync(app.Configuration.GetValue<int>("x", 10) + "\n" ); //10 è value fallback
+
+
+                });  //MyKey la trovi in appsettings.json
+            });
+
+            app.MapControllers();
 
             app.Run();
         }
